98b9a8c2af60d277ee1242c9a9876f8a2a450499
Thomas Laure <thomaslaure3@gmail.com>
2024-10-28 15:44:45 +0100

    feat: display post form

--- posts/post/models/post_form.py
+++ posts/post/models/post_form.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django.core.exceptions import NON_FIELD_ERRORS
 
 from .post import Post
 
@@ -6,8 +7,8 @@ class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
-        # error_messages = {
-        #     NON_FIELD_ERRORS: {
-        #         'unique_together': "%(model_name)s's %(field_labels)s are not unique."
-        #     }
-        # }
+        error_messages = {
+            NON_FIELD_ERRORS: {
+                "unique_together": "%(model_name)s's %(field_labels)s are not unique.",
+            }
+        }
--- posts/post/views.py
+++ posts/post/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import PostForm
 
 # Create your views here.
 def post_form(request):
-    return render(request, 'post_form.html')
+    form = PostForm(request.POST)
+    if form.is_valid():
+        form.save()
+    return render(request, 'post_form.html', {'form': form})
